261ce98e3f3380e75120db07c3fb303933d1b09c
Scala Steward <me@scala-steward.org>
2021-07-23 22:34:48 +0200

    Update micronaut-core to 2.5.11

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -3,7 +3,7 @@ import sbt._
 object Dependencies {
   object Versions {
     val kafka = "2.8.0"
-    val micronaut = "2.5.9"
+    val micronaut = "2.5.11"
     val scala3 = "3.0.1"
     val svm = "19.2.1"
   }
