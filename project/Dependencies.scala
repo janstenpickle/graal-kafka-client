5a57dd5d61ee728d4cc8e1f3cae1ec451b26f95e
Scala Steward <me@scala-steward.org>
2021-09-20 12:26:01 +0200

    Update kafka-clients to 3.0.0

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -2,7 +2,7 @@ import sbt._
 
 object Dependencies {
   object Versions {
-    val kafka = "2.8.1"
+    val kafka = "3.0.0"
     val micronaut = "3.0.1"
     val scala3 = "3.0.2"
     val svm = "19.2.1"
