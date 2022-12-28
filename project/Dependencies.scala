292f57d0d4f263194488d4e6773d4b9473eabd5e
Scala Steward <scala_steward@virtuslab.com>
2022-12-28 15:06:22 +0000

    Update micronaut-core to 3.8.0

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -3,7 +3,7 @@ import sbt._
 object Dependencies {
   object Versions {
     val kafka = "2.8.2"
-    val micronaut = "3.4.4"
+    val micronaut = "3.8.0"
     val scala3 = "3.1.2"
     val svm = "19.2.1"
   }
